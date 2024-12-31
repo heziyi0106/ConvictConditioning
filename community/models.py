84ea1cf2fb135650543c2040dae6a1dc8d7e7109
wendy_ho <wendy_ho@croxlink.com>
2024-12-31 09:57:06 +0800

    Add Models For All Apps on 2024-12-31

--- community/models.py
+++ community/models.py
@@ -1,14 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
 
 
 class Post(models.Model):
     user = models.ForeignKey(
-        User, on_delete=models.CASCADE
+        User, on_delete=models.CASCADE, verbose_name=_("使用者")
     )  # The user who created the post
-    content = models.TextField()  # The content of the post
+    content = models.TextField(_("內容"))  # The content of the post
     created_at = models.DateTimeField(
-        auto_now_add=True
+        auto_now_add=True, verbose_name=_("建立時間")
     )  # Timestamp when the post was created
 
     def __str__(self):
@@ -16,23 +17,23 @@ class Post(models.Model):
 
 
 class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)  # The related post
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, verbose_name=_("貼文"))  # The related post
     user = models.ForeignKey(
-        User, on_delete=models.CASCADE
+        User, on_delete=models.CASCADE, verbose_name=_("使用者")
     )  # The user who made the comment
-    content = models.TextField()  # The content of the comment
-    created_at = models.DateTimeField(auto_now_add=True)  # Timestamp for the comment
+    content = models.TextField(_("內容"))  # The content of the comment
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name=_("建立時間"))  # Timestamp for the comment
 
     def __str__(self):
         return f"Comment by {self.user.username} on {self.post.id}"
 
 
 class Like(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)  # The related post
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, verbose_name=_("貼文"))  # The related post
     user = models.ForeignKey(
-        User, on_delete=models.CASCADE
+        User, on_delete=models.CASCADE, verbose_name=_("使用者")
     )  # The user who liked the post
-    created_at = models.DateTimeField(auto_now_add=True)  # Timestamp for the like
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name=_("建立時間"))  # Timestamp for the like
 
     def __str__(self):
-        return f"{self.user.username} liked {self.post.id}"
+        return f"Like by {self.user.username} on {self.post.id}"
